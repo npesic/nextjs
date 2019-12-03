3b53c874fe640b645f8e3f069f360d9064017e49
fire10 <nebojsa.pesic@gmail.com>
2019-12-02 23:07:16 -0800

    back to plain js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(React.createElement('h1', false, 'Hello again from React!'), document.getElementById('root'))
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry:  __dirname + "/src/index.js",
-  output: {
-   path: __dirname + "/public",
-   filename: "bundle.js",
-   publicPath: "/",
-  }
-};
